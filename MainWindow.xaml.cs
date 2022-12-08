6e3f749b37225f030d64b0da1008efafb45d7659
NyiteX <kitheus09@gmail.com>
2022-12-08 12:54:49 +0200

    dasd

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -20,12 +20,14 @@ namespace rozetka_wpf
     /// </summary>
     public partial class MainWindow : Window
     {
+        string tmpTB = "";
         public MainWindow()
         {
             InitializeComponent();
 
             main_window.WindowStartupLocation = WindowStartupLocation.CenterScreen;
             Ini_listBox();
+            borderItem.Visibility = Visibility.Hidden;
         }
 
         private void Ini_listBox()
@@ -35,126 +37,6 @@ namespace rozetka_wpf
         private void ListBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
             
-        }
-
-        private void menu_1_MouseEnter(object sender, MouseEventArgs e)
-        {
-            menu_1.Foreground = Brushes.Tomato;
-        }
-
-        private void menu_1_MouseLeave(object sender, MouseEventArgs e)
-        {
-            menu_1.Foreground = Brushes.Black;
-        }
-
-        private void menu_2_MouseEnter(object sender, MouseEventArgs e)
-        {
-            menu_2.Foreground = Brushes.Tomato;
-        }
-
-        private void menu_2_MouseLeave(object sender, MouseEventArgs e)
-        {
-            menu_2.Foreground = Brushes.Black;
-        }
-
-        private void menu_3_MouseEnter(object sender, MouseEventArgs e)
-        {
-            menu_3.Foreground = Brushes.Tomato;
-        }
-
-        private void menu_3_MouseLeave(object sender, MouseEventArgs e)
-        {
-            menu_3.Foreground = Brushes.Black;
-        }
-
-        private void menu_4_MouseEnter(object sender, MouseEventArgs e)
-        {
-            menu_4.Foreground = Brushes.Tomato;
-        }
-
-        private void menu_4_MouseLeave(object sender, MouseEventArgs e)
-        {
-            menu_4.Foreground = Brushes.Black;
-        }
-
-        private void menu_5_MouseEnter(object sender, MouseEventArgs e)
-        {
-            menu_5.Foreground = Brushes.Tomato;
-        }
-
-        private void menu_5_MouseLeave(object sender, MouseEventArgs e)
-        {
-            menu_5.Foreground = Brushes.Black;
-        }
-
-        private void menu_6_MouseEnter(object sender, MouseEventArgs e)
-        {
-            menu_6.Foreground = Brushes.Tomato;
-        }
-
-        private void menu_6_MouseLeave(object sender, MouseEventArgs e)
-        {
-            menu_6.Foreground = Brushes.Black;
-        }
-
-        private void menu_7_MouseEnter(object sender, MouseEventArgs e)
-        {
-            menu_7.Foreground = Brushes.Tomato;
-        }
-
-        private void menu_7_MouseLeave(object sender, MouseEventArgs e)
-        {
-            menu_7.Foreground = Brushes.Black;
-        }
-
-        private void menu_8_MouseEnter(object sender, MouseEventArgs e)
-        {
-            menu_8.Foreground = Brushes.Tomato;
-        }
-
-        private void menu_8_MouseLeave(object sender, MouseEventArgs e)
-        {
-            menu_8.Foreground = Brushes.Black;
-        }
-
-        private void menu_9_MouseEnter(object sender, MouseEventArgs e)
-        {
-            menu_9.Foreground = Brushes.Tomato;
-        }
-
-        private void menu_9_MouseLeave(object sender, MouseEventArgs e)
-        {
-            menu_9.Foreground = Brushes.Black;
-        }
-
-        private void menu_10_MouseEnter(object sender, MouseEventArgs e)
-        {
-            menu_10.Foreground = Brushes.Tomato;
-        }
-
-        private void menu_10_MouseLeave(object sender, MouseEventArgs e)
-        {
-            menu_10.Foreground = Brushes.Black;
-        }
-
-        private void menu_11_MouseEnter(object sender, MouseEventArgs e)
-        {
-            menu_11.Foreground = Brushes.Tomato;
-        }
-
-        private void menu_11_MouseLeave(object sender, MouseEventArgs e)
-        {
-            menu_11.Foreground = Brushes.Black;
-        }
-
-        private void menu_12_MouseEnter(object sender, MouseEventArgs e)
-        {
-            menu_12.Foreground = Brushes.Tomato;
-        }
-
-        private void menu_12_MouseLeave(object sender, MouseEventArgs e)
-        {
-            menu_12.Foreground = Brushes.Black;
         }
 
         private void ru_label_MouseEnter(object sender, MouseEventArgs e)
@@ -175,6 +57,32 @@ namespace rozetka_wpf
         private void ua_label_MouseLeave(object sender, MouseEventArgs e)
         {
             ua_label.Foreground = Brushes.White;
+        }
+
+        private void poisk_textBox_GotFocus(object sender, RoutedEventArgs e)
+        {
+            poisk_textBox.Text = tmpTB;
+        }
+        private void poisk_textBox_LostFocus(object sender, RoutedEventArgs e)
+        {
+            tmpTB = poisk_textBox.Text;
+            if(tmpTB.Length == 0 )
+            poisk_textBox.Text = "Я шукаю...";
+        }
+
+        private void catalog_btn_Click(object sender, RoutedEventArgs e)
+        {
+            if(borderItem.Visibility== Visibility.Hidden)
+            borderItem.Visibility = Visibility.Visible;
+            else borderItem.Visibility = Visibility.Hidden;
+        }
+
+        private void borderItem_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            if (!panel_menu2.IsMouseOver)
+            {
+                borderItem.Visibility = Visibility.Hidden;
+            }
         }
     }
 }
